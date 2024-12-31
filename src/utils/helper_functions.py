e7c30f5b02afb3b183c7168bf90687423e0b1bb2
bkiyota <brettckiyota@gmail.com>
2025-01-01 05:55:56 +0900

    refactoring tutorial notebooks

--- src/utils/helper_functions.py
+++ src/utils/helper_functions.py
@@ -22,7 +22,7 @@ def instantiate_logger():
         "handlers": {
             "console": {
                 "class": "logging.StreamHandler",
-                "level": "DEBUG", 
+                "level": "INFO", 
                 "formatter": "default",
                 "stream": "ext://sys.stdout" 
             },
