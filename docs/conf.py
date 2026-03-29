364b042c670a926eebd961bc74e5f52b7be2312d
bkiyota <brettckiyota@gmail.com>
2026-03-29 15:41:11 +0900

    documentation theme

--- docs/conf.py
+++ docs/conf.py
@@ -16,6 +16,7 @@ release = '1.0.1'
 
 extensions = [
     "myst_parser",  # enables Markdown support
+    'sphinx_rtd_theme'
 ]
 
 templates_path = ['_templates']
@@ -30,5 +31,6 @@ source_suffix = {
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 html_static_path = ['_static']
