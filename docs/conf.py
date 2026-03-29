2757d4878c450d7d7d3728b31a1073599bd00d2b
bkiyota <brettckiyota@gmail.com>
2026-03-29 15:47:05 +0900

    documentation updated theme

--- docs/conf.py
+++ docs/conf.py
@@ -31,6 +31,11 @@ source_suffix = {
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# html_theme = 'alabaster'
 html_theme = "sphinx_rtd_theme"
 html_static_path = ['_static']
+
+html_theme_options = {
+    'navigation_depth': 4,    # how many levels to show in sidebar
+    'collapse_navigation': False,
+    'sticky_navigation': True
+}
